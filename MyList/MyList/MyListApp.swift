d4efaf3cdb212c74734e12bfee101ae8e04656bd
awasson1 <47997743+awasson1@users.noreply.github.com>
2023-04-12 00:48:30 -0700

    Initial Commit

---
+++ MyList/MyList/MyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyListApp.swift
+//  MyList
+//
+//  Created by Anthony Wasson on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
